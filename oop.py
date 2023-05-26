315dd9fa3c091ad373a036387cd0ff8984e9c176
hunderaweke <hunderaweke@gmail.com>
2023-05-26 17:21:56 +0300

    Fixed:function errors

--- oop.py
+++ oop.py
@@ -1,7 +1,7 @@
 # Author: Hundera Awoke
 # Date: 26/5/2023 G.C.
 # Copyright
-class Calendar:
+class BahireHasab:
     def __init__(self, ametemihret) -> None:
         self.ametemihret = ametemihret
         self.amete_alem = self.ametemihret + 5500
@@ -18,7 +18,7 @@ class Calendar:
             "ግንቦት": 9,
             "ሰኔ": 10,
             "ሐምሌ": 11,
-            "ነሀሴ": 12,
+            "ነሐሴ": 12,
             "ጳጉሜን": 13,
         }
         self.wengelawi_key = self.amete_alem % 4
@@ -29,7 +29,46 @@ class Calendar:
         if self.abekte > 30:
             self.abekte %= 30
         self.metk = abs(30 - self.abekte)
+        self.beale_metk = self.find_beale_metk()
+        self.mebaja_hamer = self.find_mebaja_hamer()
         self.tnte_kemer = ((self.amete_alem // 4) + self.amete_alem) % 7
+        self.neneweh = self.find_neneweh()
+        self.abiy_tsome = self.find_abiy_tsome()
+        self.hosaena = self.find_hosaena()
+        self.seklet = self.find_seklet()
+        self.tnsae = self.find_tnsae()
+        self.rkbe_kahnat= self.find_rkbe_kahnat()
+        self.erget = self.find_erget()
+        self.beale_hamsa = self.find_beale_hamsa()
+        self.tsome_hawaryat = self.find_tsome_hawaryat()
+        self.tsome_dihinet = self.find_tsome_dhnet()
+        self.gena = (
+            f"{self.find_day_name('ታህሳስ 29')} ታህሳስ 29"
+            if self.wengelawi != "ዘመነ ዮሐንስ"
+            else f"{self.find_day_name('ታህሳስ 28')} ታህሳስ 28"
+        )
+        self.timket = f"{self.find_day_name('ጥር 11')} ጥር 11"
+        self.kana_zegelila = f"{self.find_day_name('ጥር 12')} ጥር 12"
+        self.meskel = f"{self.find_day_name('መስከረም 17')} መስከረም 17"
+        self.beale_tsinset = f"{self.find_day_name('መጋቢት 29')} መጋቢት 29"
+        self.beale_simeon = f"{self.find_day_name('የካቲት 8')} የካቲት 8"
+        self.beale_gizret = f"{self.find_day_name('ጥር 6')} ጥር 6"
+        self.genbot_lideta = f"{self.find_day_name('ግንቦት 1')} ግንቦት 1"
+        self.debre_tabor = f"{self.find_day_name('ነሐሴ 13')} ነሐሴ 13"
+        self.tsome_lidet = (
+            f"{self.find_day_name('ህዳር 15')} ህዳር 16"
+            if self.wengelawi!= "ዘመነ ዮሐንስ"
+            else f"{self.find_day_name('ህዳር 15')} ህዳር 15"
+        )
+        self.day_of_timket = self.find_day_name("ጥር 11")
+        self.tsome_gehad = (
+            f"{self.find_day_name('ጥር 10')} ጥር 10"
+            if self.day_of_timket == "አርብ" or self.day_of_timket == "ረቡዕ"
+            else "የለም"
+        )
+        self.tsome_filseta = f"{self.find_day_name('ነሐሴ 1')} ነሐሴ 1"
+        self.tsome_filseta_mefchiya = f"{self.find_day_name('ነሐሴ 16')} ነሐሴ 16"
+        self.debre_zeyit = self.find_debrezeyt()
 
     def find_beale_metk(self):
         """A Function For Findein"""
@@ -37,7 +76,7 @@ class Calendar:
         if metk > 30:
             metk = metk - 30
         if metk >= 15 and metk <= 30:
-            self.beale_metk = f"መስክረም {metk}"
+            self.beale_metk = f"መስከረም {metk}"
         elif metk >= 2 and metk <= 14:
             self.beale_metk = f"ጥቅምት{metk}"
         return self.beale_metk
@@ -58,7 +97,7 @@ class Calendar:
     def find_mebaja_hamer(self) -> int:
         beale_metk = self.beale_metk
         TEWSAK = {"ቅዳሜ": 8, "እሁድ": 7, "ሰኞ": 6, "ማግሰኞ": 5, "ረቡዕ": 4, "ሐሙስ": 3, "አርብ": 2}
-        day = Calendar.find_day_name(self, beale_metk)
+        day = self.find_day_name(month_date_string= beale_metk)
         metk = self.metk
         tewsak = TEWSAK[day]
         self.mebaja_hamer = tewsak + metk if tewsak + metk < 30 else tewsak
@@ -86,6 +125,7 @@ class Calendar:
         else:
             month = "የካቲት"
         self.neneweh = f"{month} {self.mebaja_hamer}"
+        return self.neneweh
 
     def find_abiy_tsome(self) -> str:
         "A Function for finding the day of enterance of the Great lent according to the Ethiopian Calendar."
@@ -122,15 +162,15 @@ class Calendar:
         date = int(date)
         sklet_date = date + 5
         if sklet_date > 30:
-            sklet_date -= 30
-        self.sklet = f"{month} {date}"
+            sklet_date %= 30
+        self.sklet = f"{month} {sklet_date}"
         return self.sklet
 
     def find_tnsae(self) -> str:
         "A function for calculating the date of Tnsae(The end of Great lent) in Ethiopian Calendar."
-        month, date = [_ for _ in self.sklet.split()]
+        month, date = [_ for _ in self.hosaena.split()]
         date = int(date)
-        tnsae_date = date + 2
+        tnsae_date = date + 7
         self.tnsae = f"{month} {tnsae_date}"
         return self.tnsae
 
@@ -168,14 +208,14 @@ class Calendar:
     def find_beale_hamsa(self):
         "A function for calculating the date of Beale hamsa in Ethiopian Calendar."
         month,date = [_ for _ in self.erget.split()]
-        month = int(month)
-        beale_hamsa_date = month + 10
-        if month == "ግንቦት" and 20 >= month >= 5:
-            bealeHamsaWer = "ግንቦት"
+        date = int(date)
+        beale_hamsa_date = date + 10
+        if date == "ግንቦት" and 20 >= date >= 5:
+            beale_hamsa_month = "ግንቦት"
         else:
-            bealeHamsaWer = "ሰኔ"
+            beale_hamsa_month = "ሰኔ"
         beale_hamsa_date %= 30
-        self.beale_hamsa = f"{bealeHamsaWer} {beale_hamsa_date}"
+        self.beale_hamsa = f"{beale_hamsa_month} {beale_hamsa_date}"
         return self.beale_hamsa
     def find_tsome_hawaryat(self):
         "A function for finding the date of Tsome Hawaryat (Lent of The Apostels) in Ethiopian Calendar"
@@ -199,9 +239,4 @@ class Calendar:
             tsome_dhnet_month = month
         self.tsome_dhnet = f"{tsome_dhnet_month} {tsome_dhnet_date}"
         return self.tsome_dhnet
-    def find_tsome_filseta(self):
-        date_meyazha = "ነሀሴ 1"
-        date_mefcha = "ነሀሴ 16"
-        self.day_mefcha = Calendar.find_day_name(date_mefcha)
-        self.day_meyazha = Calendar.find_day_name(date_meyazha)
-        return self.day_meyazha, self.day_mefcha
+
---
+++ test.py
@@ -0,0 +1,4 @@
+from oop import *
+year = int(input())
+bh = BahireHasab(year)
+print(bh.sklet)
