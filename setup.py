6d087b3c930308014cdc642c762d8d145198e4c8
hunderaweke <hunderaweke@gmail.com>
2023-05-26 21:48:57 +0300

    Finished:config for pypi.org

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup,find_packages
+with open("./README.mdhVfAVmUY38chFqshVfAVmUY38chFqs","r") as file:
+    long_description = file.read()
 setup(
     name='bahire-hasab',
     author="Hundera Awoke",
     version="0.0.1.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email="hunderaweke@gmail.com",
     url="https://github.com/hunderaweke/bahire-hasab",
     packages=find_packages('src'),
